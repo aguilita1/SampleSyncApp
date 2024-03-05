8fb84d937140038782dd521956a2187a190a0c95
Daniel Kelley <freefln.cowboy@gmail.com>
2024-03-05 23:45:17 +0000

    Basic Sync App written in PHP

---
+++ Dockerfile
@@ -0,0 +1,85 @@
+# Stage 1
+# Run composer
+FROM  composer:2.7.1 as composer
+WORKDIR /app
+COPY ./composer.json /app
+COPY ./composer.lock /app
+
+# Copy the current directory contents into the container deployment folder
+COPY ./src /app/lib
+RUN composer install --no-interaction --no-dev --ignore-platform-reqs --optimize-autoloader
+
+# Tidy up
+# remove non-required vendor files and anything else we do not want in the archive and do not need for
+# the next state
+RUN rm /app/composer.*
+
+# Stage 2
+# Build the IR container
+# Extend from alpine parent image
+FROM alpine:3.18.6
+
+MAINTAINER Sample Sync App <Daniel.Ian.Kelley@gmail.com>
+
+# Added meta-data about this app
+ARG APP_VERSION="1.0.0"
+LABEL vendor=REVOLVE \
+      com.github.aguilita1.is-beta="false" \
+      com.github.aguilita1.is-production="true" \
+      com.github.aguilita1.version=$APP_VERSION \
+      com.github.aguilita1.release-date="2024-03-05"
+
+# Install apache, PHP, and supplimentary programs.
+RUN apk update && apk upgrade && apk add \
+    libcrypto1.1 \
+    libssl1.1 \
+    tar \
+    bash \
+    curl \
+    php82 \
+    php82-curl \
+    php82-session \
+    php82-fileinfo \
+    php82-xml \
+    php82-simplexml \
+    tzdata \
+    && rm -rf /var/cache/apk/*
+
+# Add all necessary config files in one layer
+ADD docker/ /
+
+# Update the PHP.ini file
+RUN sed -i "s/error_reporting = .*$/error_reporting = E_ERROR | E_WARNING | E_PARSE/" /etc/php82/php.ini && \
+    sed -i "s/session.gc_probability = .*$/session.gc_probability = 50/" /etc/php82/php.ini && \
+    sed -i "s/session.gc_divisor = .*$/session.gc_divisor = 100/" /etc/php82/php.ini
+
+# Setup persistent environment variables
+ENV SA_PHP_SESSION_GC_MAXLIFETIME=1440 \
+    SA_PHP_MAX_EXECUTION_TIME=300 \
+    SA_PHP_MEMORY_LIMIT=256M \
+    SA_TIME_ZONE=America/New_York \
+    SA_SYNC_INTERVAL=120 \
+    SA_START_SYNC=04:00:00\
+    SA_STOP_SYNC=23:59:59
+
+
+# Map the source files into /var/www/cms
+RUN mkdir -p /opt/ir
+COPY --from=composer /app /opt/ir
+
+
+# Write App version to settings.php file
+RUN sed -i "s/.*app_version = 'APP_VERSION'.*$/\$app_version = '$APP_VERSION';/" /opt/ir/lib/main.php
+
+# Map a volumes to this folder.
+# Our CMS files, library, cache and backups will be in here.
+RUN chown -R nobody /opt/ir && \
+    chmod +x /entrypoint.sh
+
+#Set Time Zone
+RUN ln -snf /usr/share/zoneinfo/$SA_TIME_ZONE /etc/localtime && echo $SA_TIME_ZONE > /etc/timezone
+
+# Set the working directory to root
+WORKDIR /
+
+CMD ["/entrypoint.sh"]
---
+++ src/main.php
@@ -0,0 +1,88 @@
+#!/usr/bin/env php
+<?php
+/*
+ * Sample Sync Application written in PHP to test out Github Actions.
+ * Copyright (C) 2024 Daniel Kelley
+ * (main.php)
+ */
+define('PROJECT_ROOT', realpath(__DIR__ . '/..'));
+
+require PROJECT_ROOT . '/vendor/autoload.php';
+
+    global $app_version;
+	$app_version = 'APP_VERSION';
+
+    global $syncStart;
+    global $syncEnd;
+        global $delayedRetryTime;
+    //Define synchronization environment variables
+    $syncInterval= $_SERVER['SA_SYNC_INTERVAL'];
+    $syncStart= $_SERVER['SA_START_SYNC'];
+    $syncEnd= $_SERVER['SA_STOP_SYNC'];
+
+    //session_start();
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    global $increment;
+    $increment = 0;
+try {
+// [0] initialize
+    // Set up logging to file
+        $log = new \Monolog\Logger('SampleSyncApp');
+    $stream = new \Monolog\Handler\StreamHandler(fopen('php://stdout', 'w'), \Monolog\Level::Debug);
+    $stream->setFormatter(new \Monolog\Formatter\JsonFormatter());
+    $log->pushHandler($stream);
+    $log->info(sprintf('Starting up - SampleSyncApp version: %s PROJECT_ROOT=%s', $GLOBALS['app_version'], PROJECT_ROOT));
+
+    $log->debug('*****************START**main.php*********************');
+
+
+// [1]
+    $loop = React\EventLoop\Factory::create();
+
+// [2]
+    $loop->addPeriodicTimer($syncInterval, function () use ($log, $syncInterval, $syncStart, $syncEnd) {
+
+        $dateToCompare = date('H:i:s');
+        try {
+            if ($dateToCompare >= $syncStart && $dateToCompare <= $syncEnd) {
+                $log->info(sprintf("Tick - %d more seconds. Inside sync interval, start = %s and end = %s.", $syncInterval, $syncStart, $syncEnd),
+                    array('memoryAllocated' => memory_get_usage(), 'peakMemoryAllocated' => memory_get_peak_usage()));
+                Sync($log);
+                gc_collect_cycles();
+                gc_mem_caches();
+            } else {
+                $log->info(sprintf("Tick - skipped. Outside sync interval, start = %s and end = %s.", $syncStart, $syncEnd));
+            }
+        } catch (GuzzleHttp\Exception\ConnectException $e) {
+            $log->error('Connect Exception: ' . $e->getMessage() . $e->getTraceAsString());
+            error_log('Connect Exception:' . $e->getMessage() . $e->getTraceAsString() . PHP_EOL);
+        } catch (GuzzleHttp\Exception\RequestException $e3) {
+            $log->error('Request Exception: ' . $e3->getMessage() . $e3->getTraceAsString());
+            error_log('Request Exception:' . $e3->getMessage() . $e3->getTraceAsString() . PHP_EOL);
+        } catch (TypeError $e4) {
+            $log->error('TypeError Exception: ' . $e4->getMessage() . $e4->getTraceAsString());
+            error_log('TypeError Exception:' . $e4->getMessage() . $e4->getTraceAsString() . PHP_EOL);
+        }
+
+    });
+
+// [3]
+    $loop->run();
+}catch(Exception $ex){
+    error_log('Unknown Exception:' . $ex->getMessage() . $ex->getTraceAsString() . PHP_EOL);
+}
+
+/**
+ * @param \Monolog\Logger
+  */
+function Sync(\Monolog\Logger $log )
+{
+     $todayDt = new DateTime();
+    $endDt = (new DateTime())->add(date_interval_create_from_date_string('1 days'));
+    $log->info('Synchronizing data between ' . $todayDt->format("Y-m-d") . ' and '. $endDt->format("Y-m-d").'.');
+    $log->info('Please wait... doing meaningless pretend work.');
+    $log->info('Synchronization is complete.');
+}
+
+?>
